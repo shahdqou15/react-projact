968a70f5ecbe7ddb878eb9614d62d1a5621b9d03
shahdqou15 <shahd.qou15@gmail.com>
2026-01-16 21:34:50 +0200

    create about section

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -5,7 +5,13 @@ function About() {
 
   return (
     <>
-    <h1>about</h1>
+    <section className={style.about}>
+      <div className="container">
+        <h2>We've got what you need!</h2>
+        <p>Start Bootstrap has everything you need to get your new website up and running in no <br></br>time! Choose one of our open source, free to download, and easy to use themes! No<br></br> strings attached!</p>
+        <button className={style.btnStart}>GET STARTED!</button>
+      </div>
+    </section>
 
     </>
   )
