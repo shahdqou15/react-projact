1783f2627f95ddf0fcab34c43e67435b1bbac969
shahdqou15 <shahd.qou15@gmail.com>
2026-01-16 21:18:47 +0200

    create hero section

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import style from "./hero.module.css"
-function Hero() {
 
+function Hero() {
 
   return (
     <>
-<h1 className="bg-danger">hero</h1>
+    <div className={style.hero}>
+      <div className="container">
+        <h1>Your Favorite Place for<br></br> Free Bootstrap Themes</h1>
+        <p>Start Bootstrap can help you build better websites using the Bootstrap<br></br> framework! Just download a theme and start customizing, no strings<br></br> attached!</p>
+        <button className={style.btn}>FIND OUT MORE</button>
+      </div>
+    </div>
+
     </>
   )
 }
