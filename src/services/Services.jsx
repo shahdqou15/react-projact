fb2a839d5d4e9b2f55723a469d5598209c57e0f8
shahdqou15 <shahd.qou15@gmail.com>
2026-01-16 22:11:36 +0200

    create services section

--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -5,7 +5,33 @@ function Services() {
 
   return (
     <>
-        <h1>services</h1>
+      <section className={style.services}>
+        <div className="container">
+          <h2 className="pb-5">At Your Service</h2>
+          <div className="row">
+            <div className="col-md-3">
+              <i className="fa-solid fa-gem fa-lg" style={{ color: "#f4623a" }}></i>
+              <h5 className="fs-4">Sturdy Themes</h5>
+              <p>Our themes are updated regularly to keep them bug free!</p>
+            </div>
+            <div className="col-md-3">
+              <i className="fa-solid fa-laptop fa-lg" style={{ color: "#f4623a" }}></i>
+              <h5 className="fs-4">Up to Date</h5>
+              <p>All dependencies are kept current to keep things fresh.</p>
+            </div>
+            <div className="col-md-3">
+              <i className="fa-solid fa-globe fa-lg" style={{ color: "#f4623a" }}></i>
+              <h5 className="fs-4">Ready to Publish</h5>
+              <p>You can use this design as is, or you can make changes!</p>
+            </div>
+            <div className="col-md-3">
+              <i className="fa-regular fa-heart fa-lg" style={{ color: "#f4623a" }}></i>
+              <h5 className="fs-4">Made with Love</h5>
+              <p>Is it really open source if it's not made with love?</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
