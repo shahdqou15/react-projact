3a4779fb45cad6cd920a658e77e1e766b860d553
shahdqou15 <shahd.qou15@gmail.com>
2026-01-16 14:34:53 +0200

    navbar section

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -6,7 +6,22 @@ function Navbar() {
 
   return (
     <>
-        <h1>navbar</h1>
+      <div className={style.navbar}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <a href="#" className="text-decoration-none">Start Bootstrap</a>
+            </div>
+            <div className="col-md-8">
+              <ul className="d-flex gap-5 ">
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
